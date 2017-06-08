c95654718ec66d800f0c0d4e413b292ea9f8e25f
harshaonhub28 <hvk.2803@gmail.com>
2017-06-08 19:37:44 +0530

    multiple screens

--- src/components/list.js
+++ src/components/list.js
@@ -1,15 +1,49 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const List = ({ todos, toggle }) => {
-  const list = todos.map(todo => {
-    if (!todo.isDone) {
-      return <li onClick={e => toggle(todo)}>{todo.task}</li>;
+class List extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { display: 'All' };
+  }
+
+  render() {
+    const list_all = this.props.todos.map(todo => {
+      if (!todo.isDone) {
+        return <li onClick={e => this.props.toggle(todo)}>{todo.task}</li>;
+      } else {
+        return (
+          <li onClick={e => this.props.toggle(todo)}><s> {todo.task} </s></li>
+        );
+      }
+    });
+
+    const list_active = this.props.todos.filter(todo => todo.isDone);
+    const list_inactive = this.props.todos.filter(todo => !todo.isDone);
+
+    let list = [];
+
+    if (this.state.display === 'All') {
+      list = list_all;
+    } else if (this.state.display === 'Active') {
+      list = list_active;
     } else {
-      return <li onClick={e => toggle(todo)}><s> {todo.task} </s></li>;
+      list = list_inactive;
     }
-  });
 
-  return <ul>{list}</ul>;
-};
+    return (
+      <div>
+        <button onClick={() => this.setState({ display: 'All' })}> All </button>
+        <button onClick={() => this.setState({ display: 'Active' })}>
+          {' '}Active{' '}
+        </button>
+        <button onClick={() => this.setState({ display: 'Inactive' })}>
+          {' '}Inactive{' '}
+        </button>
+        <ul>{list}</ul>
+      </div>
+    );
+  }
+}
 
 export default List;
